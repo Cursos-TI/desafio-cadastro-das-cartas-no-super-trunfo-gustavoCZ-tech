c5630e8b2c170af91b5a1df44c41e13025859ec5
gustavo_CZ-tech <gustavo.tec1211@gmail.com>
2025-01-26 19:06:45 +0000

    alteração final da base do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,12 +8,9 @@
 
 int main(){
     
-    char pais[20];
-    char estadoA[30], estadoB[30], estadoC[30], estadoD[30], estadoE[30], estadoF[30], estadoG[30], estadoH[30];
-    char cidade1[30],cidade2[30],cidade3[30],cidade4[30];
-    char dados1[20];
+    char pais[20], estado[20], cidade[30], dados[20], codigo_da_cidade[20];
 
-    printf("Bem vindo ao Jogo de cartoas super trunfo\n");
+    printf("Bem vindo ao Jogo de cartas super trunfo\n");
 
    //entrada de dados 
    //digita o nome de um pais
@@ -23,51 +20,41 @@ int main(){
 
    //entra com o valor para estado 
     printf("Digite um estado: ");
-    scanf("%s", &estadoA);
+    scanf("%s", &estado);
 
     //etra com um valor para cidade
     printf("Digite uma cidade: ");
-    scanf("%s", &cidade1);
+    scanf("%s", &cidade);
+
+    //entrar com o codigo da carta
+    printf("Digite o codigo da cidade: ");
+    scanf("%s", codigo_da_cidade);
 
     //entra com as informações de(cidade) -> população, área, pib, nimero de pontos turistico
 
     //pop(populção), tur(pontos turistico), 
-    int pop, tur;
+    int populacao, ponto_turisticos;
     float area, PIB;
 
     printf("Quantas pessoal tem na cidade: ");
-    scanf("%d", &pop );
+    scanf("%d", &populacao );
 
-    printf("Quanto e a Àrea da cidade: ");
-    scanf("%.2f", &area);
+    printf("Quanto e a Àrea da cidade em km²: ");
+    scanf("%f", &area);
 
     printf("Quanto e o PIB: ");
     scanf("%f", &PIB);
 
     printf("quantos pontos turisticos ela tem: ");
-    scanf("%d", &tur);
+    scanf("%d", &ponto_turisticos);
+
+    printf("--------------------------------------------------------------\n");
+    printf("estado: %s\n Codigo da carta: %s\n Nome da cidade: %s\n População: %d\n ", estado, codigo_da_cidade, cidade, populacao);
+    printf("Área em km²: %.2f\n PIB: %.2f\n Número de pontos turísticos: %d\n\n", area , PIB, ponto_turisticos );
 
     return 0;
+    
+    
 
-    // nostra os valore em uma mesma linha da carta cidade
-    //
-    
-    
-    
-    
-    
-    
-    
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-   
 }
